f3ed6cac994e14088d1facb6a737910ee8892f38
Conrado Jordan <conrado.jordan@gmail.com>
2024-05-30 11:45:46 -0300

    New combat formula, rebalance monsters and loot

--- game/js/items.js
+++ game/js/items.js
@@ -1,4 +1,4 @@
-const all_items = [
+const gold = [
     {
         id: 1,
         name: "gold coin",
@@ -6,61 +6,69 @@ const all_items = [
         attack: 0,
         defense: 0,
         value: 1
-    },
+    }
+]
+
+const weapons = [
     {
         id: 1000,
-        name: "Small Knife",
+        name: "Short Sword",
         type: "weapon",
-        attack: 5,
+        attack: 2,
         value: 10
     },
     {
         id: 1001,
         name: "Diego's Club",
         type: "weapon",
-        attack: 6,
-        value: 15
+        attack: 4,
+        value: 20
     },
     {
         id: 1002,
         name: "Katana",
         type: "weapon",
-        attack: 8,
-        value: 25
+        attack: 5,
+        value: 45
     },
     {
         id: 1003,
         name: "Scorpion's Claw",
         type: "weapon",
-        attack: 12,
+        attack: 8,
         value: 120
     },
     {
         id: 1004,
         name: "Staff of Falerga",
         type: "weapon",
-        attack: 16,
+        attack: 12,
         value: 180
-    },
+    }
+]
+
+const shields = [
     {
         id: 2000,
         name: "Wooden Shield",
         type: "shield",
-        defense: 10,
+        defense: 2,
         value: 10
     },
     {
         id: 2001,
         name: "Iron Shield",
         type: "shield",
-        defense: 13,
+        defense: 4,
         value: 18
     },
     {
         id: 2002,
         name: "Shield of Dispute",
         type: "shield",
-        defense: 15,
+        defense: 7,
         value: 30
     },
 ]
+
+const ALL_ITEMS = gold.concat(weapons, shields)
