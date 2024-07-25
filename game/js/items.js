f50b08b429f309dc0c683fb28d3d22265f89648d
Matheus Henrique <matheus.181137@gmail.com>
2024-07-24 22:08:03 -0300

    feat: subtract gold from player when it has enough gold

--- game/js/items.js
+++ game/js/items.js
@@ -1,81 +1,83 @@
 const gold = [
-    {
-        id: 1,
-        name: "gold coin",
-        type: "gold",
-        attack: 0,
-        defense: 0,
-        value: 1
-    }
-]
+  {
+    id: 1,
+    name: "gold coin",
+    type: "gold",
+    attack: 0,
+    defense: 0,
+    value: 1,
+  },
+];
 
 const weapons = [
-    {
-        id: 1000,
-        name: "Small Knife",
-        type: "weapon",
-        attack: 1,
-        value: 5
-    },
-    {
-        id: 1001,
-        name: "Short Sword",
-        type: "weapon",
-        attack: 2,
-        value: 10
-    },
-    {
-        id: 1002,
-        name: "Diego's Club",
-        type: "weapon",
-        attack: 4,
-        value: 20
-    },
-    {
-        id: 1003,
-        name: "Katana",
-        type: "weapon",
-        attack: 5,
-        value: 45
-    },
-    {
-        id: 1004,
-        name: "Scorpion's Claw",
-        type: "weapon",
-        attack: 8,
-        value: 120
-    },
-    {
-        id: 1005,
-        name: "Staff of Falerga",
-        type: "weapon",
-        attack: 12,
-        value: 180
-    }
-]
+  {
+    id: 1000,
+    name: "Small Knife",
+    type: "weapon",
+    attack: 1,
+    value: 5,
+  },
+  {
+    id: 1001,
+    name: "Short Sword",
+    type: "weapon",
+    attack: 2,
+    value: 10,
+  },
+  {
+    id: 1002,
+    name: "Diego's Club",
+    type: "weapon",
+    attack: 4,
+    value: 20,
+  },
+  {
+    id: 1003,
+    name: "Katana",
+    type: "weapon",
+    attack: 5,
+    value: 45,
+  },
+  {
+    id: 1004,
+    name: "Scorpion's Claw",
+    type: "weapon",
+    attack: 8,
+    value: 120,
+  },
+  {
+    id: 1005,
+    name: "Staff of Falerga",
+    type: "weapon",
+    attack: 12,
+    value: 180,
+  },
+];
 
 const shields = [
-    {
-        id: 2000,
-        name: "Wooden Shield",
-        type: "shield",
-        defense: 2,
-        value: 10
-    },
-    {
-        id: 2001,
-        name: "Iron Shield",
-        type: "shield",
-        defense: 4,
-        value: 18
-    },
-    {
-        id: 2002,
-        name: "Shield of Dispute",
-        type: "shield",
-        defense: 7,
-        value: 30
-    },
-]
+  {
+    id: 2000,
+    name: "Wooden Shield",
+    type: "shield",
+    defense: 2,
+    value: 10,
+  },
+  {
+    id: 2001,
+    name: "Iron Shield",
+    type: "shield",
+    defense: 4,
+    value: 18,
+  },
+  {
+    id: 2002,
+    name: "Shield of Dispute",
+    type: "shield",
+    defense: 7,
+    value: 30,
+  },
+];
 
-const ALL_ITEMS = gold.concat(weapons, shields)
+const merchantItems = [...shields, ...weapons];
+
+const ALL_ITEMS = gold.concat(weapons, shields);
--- game/js/shop.js
+++ game/js/shop.js
@@ -1,4 +1,48 @@
-character = getData("character");
+let character;
 
-let gold = document.querySelector("h3");
-gold.innerText = `💰 ${character.gold}  (poor)`;
+const getCharacterData = () => (character = getData("character"));
+const safeSubtract = (value1, value2) => Number(value1) - Number(value2);
+const saveCharacter = (character) => setData("character", character);
+const setGold = () => {
+  let gold = document.querySelector("h3");
+  gold.innerText = `💰 ${character.gold}  (poor)`;
+};
+
+const startShop = () => {
+  getCharacterData();
+  setGold();
+
+  let itemsSelect = document.querySelector("select");
+  for (const item of merchantItems) {
+    let itemsOptions = document.createElement("option");
+    itemsOptions.value = item.value;
+    itemsOptions.innerText = `${item.name} (${item.attack ? "atk" : "def"}: ${
+      item.attack ? item.attack : item.defense
+    }) Value: ${item.value}`;
+    itemsSelect.appendChild(itemsOptions);
+  }
+
+  const buttons = document.querySelectorAll(".link__button-styled");
+
+  const buyButton = Array.from(buttons).find(
+    (button) => button.textContent === "Buy"
+  );
+  buyButton.addEventListener("click", () => buyItem(itemsSelect.value));
+};
+
+const buyItem = (itemValue) => {
+  if (itemValue > character.gold) {
+    alert(
+      "You do not have enough gold! Please stop being poor and come again."
+    );
+  }
+
+  console.log(typeof character.gold);
+  console.log(typeof itemValue);
+
+  character.gold = safeSubtract(character.gold, itemValue);
+  saveCharacter(character);
+  setGold();
+};
+
+startShop();
