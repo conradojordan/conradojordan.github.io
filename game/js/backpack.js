7f69775f41e15eb14b783e8a6a7cce45ab703f4f
Matheus Henrique <matheus.181137@gmail.com>
2024-07-29 09:31:24 -0300

    feat: create buy function to shop

---
+++ game/js/backpack.js
@@ -0,0 +1,55 @@
+export const addToBackpack = (itemId, quantity, character) => {
+    let foundItems = character.backpack.filter((x) => x.id == itemId);
+    if (foundItems.length > 0) {
+        foundItems[0].quantity += quantity;
+    } else {
+        character.backpack.push({ id: itemId, quantity: quantity });
+    }
+    }
+  
+export const removeFromBackpack = (itemId, quantity) => {
+    let foundItems = character.backpack.filter((x) => x.id == itemId);
+    if (foundItems.length > 0) {
+      foundItems[0].quantity -= quantity;
+      if (foundItems[0].quantity <= 0) {
+        character.backpack = character.backpack.filter((x) => x.id != itemId);
+      }
+    }
+}
+
+const createBackpackItemElement = (itemReference) => {
+  let item = retrieveItem(itemReference.id);
+  let itemElement = document.createElement("p");
+  let itemText = `${itemReference.quantity}x ${item.name}`;
+  if (itemReference.quantity > 1) {
+    itemText += "s";
+  }
+  if (item.attack) {
+    itemText += ` (atk: ${item.attack})`;
+  }
+  if (item.defense) {
+    itemText += ` (def: ${item.defense})`;
+  }
+  itemElement.innerText = itemText;
+  return itemElement;
+}
+
+export const showCharacterBackpack = (character) => {
+  const backpackSpace = document.querySelector('#backpack-space');
+
+  if (character.backpack.length > 0) {
+    let emptyElement = document.querySelector("#backpack-empty");
+    if (emptyElement) {
+      emptyElement.remove();
+    }
+    for (let itemReference of character.backpack) {
+      let itemElement = createBackpackItemElement(itemReference);
+      backpackSpace.appendChild(itemElement);
+    }
+  } else {
+    let emptyElement = document.createElement("p");
+    emptyElement.id = "backpack-empty";
+    emptyElement.innerText = "<empty>";
+    backpackSpace.appendChild(emptyElement);
+  }
+}
--- game/js/shop.js
+++ game/js/shop.js
@@ -1,13 +1,11 @@
+import {addToBackpack, showCharacterBackpack} from './backpack.js'
+
 let character;
 
 const getCharacterData = () => (character = getData("character"));
 const safeSubtract = (value1, value2) => Number(value1) - Number(value2);
 const saveCharacter = (character) => setData("character", character);
-
-const debug = () => {
-  let debug = document.querySelector("#debug");
-  debug.innerText = JSON.parse(character.backpack);
-};
+const getMerchantItemById = itemId => merchantItems.find((item) => item.id === Number(itemId));
 
 const setGold = () => {
   let gold = document.querySelector("h3");
@@ -17,11 +15,12 @@ const setGold = () => {
 const startShop = () => {
   getCharacterData();
   setGold();
+  showCharacterBackpack(character);
 
   let itemsSelect = document.querySelector("select");
   for (const item of merchantItems) {
     let itemsOptions = document.createElement("option");
-    itemsOptions.value = item.value;
+    itemsOptions.value = item.id;
     itemsOptions.innerText = `${item.name} (${item.attack ? "atk" : "def"}: ${
       item.attack ? item.attack : item.defense
     }) Value: ${item.value}`;
@@ -36,20 +35,21 @@ const startShop = () => {
   buyButton.addEventListener("click", () => buyItem(itemsSelect.value));
 };
 
-const buyItem = (itemValue) => {
-  if (itemValue > character.gold) {
+const buyItem = (itemId) => {
+  const item = getMerchantItemById(itemId)
+  if (item.value > character.gold) {
     alert(
       "You do not have enough gold! Please stop being poor and come again."
     );
+    return;
   }
 
-  console.log(typeof character.gold);
-  console.log(typeof itemValue);
-
-  character.gold = safeSubtract(character.gold, itemValue);
+  character.gold = safeSubtract(character.gold, item.value);
+  addToBackpack(item.id, 1, character)
   saveCharacter(character);
   setGold();
-  debug();
+  showCharacterBackpack(character);
+  console.log(character.backpack)
 };
 
 startShop();
