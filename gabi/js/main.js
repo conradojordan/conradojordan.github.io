cf6fdc102a5b7003a18771e60f681ee355caa563
Conrado Jordan <conrado.jordan@gmail.com>
2025-06-27 17:41:25 -0300

    Add dado to gabi/

--- gabi/js/main.js
+++ gabi/js/main.js
@@ -1,5 +1,12 @@
 const now = new Date();
 
+// Temas
+const TEMA = "tema";
+const ORIGINAL = "original";
+const PINK = "pink";
+const DARK = "dark";
+const ECO = "eco";
+
 let periodo;
 if (now.getHours() < 12) {
     periodo = "dia";
@@ -37,7 +44,7 @@ let adjetivo = adjetivos[now.getDate() % adjetivos.length];
 let mensagem1_texto;
 switch (periodo) {
     case "dia":
-        mensagem1_texto = `Espero que tu tenha ${artigo_weekday} ${weekday} ${adjetivo}!`;
+        mensagem1_texto = `Espero que tu tenhas ${artigo_weekday} ${weekday} ${adjetivo}!`;
         break;
     case "tarde":
         mensagem1_texto = `Espero que tu estejas tendo ${artigo_weekday} ${weekday} ${adjetivo}!`;
@@ -50,6 +57,77 @@ switch (periodo) {
 let mensagem1 = document.getElementById("mensagem1");
 mensagem1.innerHTML = mensagem1_texto;
 
+
+function jogarDado() {
+    let resultadoDado = Math.floor(Math.random() * 6) + 1;
+    let resultadoDiv = document.getElementById("resultado");
+
+    resultado = `Resultado: ${resultadoDado}`;
+    if (resultadoDado === 1) {
+        resultado = resultado + " 😭";
+    } else if (resultadoDado === 6) {
+        resultado = resultado + " 🥳";
+    }
+    resultadoDiv.innerHTML = `${resultado}`;
+}
+
+let dado = document.getElementById("dado");
+dado.addEventListener("click", jogarDado);
+
+function coresDoTema(tema) {
+    switch (tema) {
+        case ORIGINAL:
+            return ["#e4e4e4", "#ff66b2", "#3a8d3d", "#555", "#111"];
+        case PINK:
+            return ["#ffccff", "#ff66b2", "#27bff7", "#a12eff", "#27bff7"];
+        case DARK:
+            return ["#080808", "#111", "#700", "#aaa", "#A00"];
+        case ECO:
+            return ["#c8e3c5", "#693526", "#552315", "#693526", "#552315"];
+        default:
+            return ["#e4e4e4", "#ff66b2", "#3a8d3d", "#555", "#111"];
+    }
+}
+
+function aplicarTema(tema) {
+    [background, color, h1color, h3color, divcolor] = coresDoTema(tema)
+
+    let body = document.body;
+    body.style.backgroundColor = background;
+    body.style.color = color;
+
+
+    let divs = document.querySelectorAll("div");
+    divs.forEach((element) => {
+        element.style.color = divcolor;
+    });
+    let h1 = document.querySelectorAll("h1");
+    h1.forEach((element) => {
+        element.style.color = h1color;
+    });
+    let h3 = document.querySelectorAll("h3");
+    h3.forEach((element) => {
+        element.style.color = h3color;
+    });
+    let h1_div = document.querySelectorAll("h1 div");
+    h1_div.forEach((element) => {
+        element.style.color = h1color;
+    });
+    let h3_div = document.querySelectorAll("h3 div");
+    h3_div.forEach((element) => {
+        element.style.color = h3color;
+    });
+}
+
+function mudarTema() {
+    let tema = document.getElementById("tema").value;
+    aplicarTema(tema);
+    localStorage.setItem(TEMA, tema);
+}
+
+let tema = localStorage.getItem(TEMA) ? localStorage.getItem(TEMA) : ORIGINAL;
+aplicarTema(tema);
+document.getElementById("tema").value = tema;
 
 // let mensagens2 = [
 //     "E que tua vida seja tão iluminada quanto os teus olhos",
